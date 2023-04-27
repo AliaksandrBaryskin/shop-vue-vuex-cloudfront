7cd1fe044828e3273d1b404767aaa8e4992fb653
Aliaksandr Baryskin <aliaksandr_baryskin@epam.com>
2023-04-27 14:29:10 +0300

    task-4 & task-5: dynamo db + s3 integration

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -2,7 +2,8 @@ export const API_PATHS = {
 	// product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	product: 'https://yjg4eaoec8.execute-api.us-east-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	// import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://saykojus99.execute-api.us-east-1.amazonaws.com/dev',
 	bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
